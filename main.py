690f122a46684bce971d95cedc832af8fecd36ff
gigcomm <lenovo27835@gmail.com>
2026-09-06 16:59:41 +0300

    feat: add get, post, patch, delete request for category

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import uuid
-from http import HTTPStatus
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Response
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from starlette import status
@@ -27,7 +26,14 @@ class TaskUpdateResponse(BaseModel):
     title: str | None = None
     completed: bool | None = None
 
+class Category(BaseModel):
+    id: str
+    name: str
 
+class CategoryCreate(BaseModel):
+    name: str
+
+categories: list[Category] = []
 tasks: list[Task] = []
 
 @app.get('/tasks')
@@ -56,3 +62,37 @@ def delete_task(task_id: str):
     for task in tasks:
         if task.id == task_id:
             tasks.remove(task)
+
+
+@app.get('/categories')
+def get_categories() -> list[Category]:
+    return categories
+
+@app.post('/categories', status_code=status.HTTP_201_CREATED)
+def create_category(data: CategoryCreate) -> Category:
+    new_category = Category(id=str(uuid.uuid4()), name=data.name)
+    categories.append(new_category)
+    return new_category
+
+@app.patch('/categories/{category_id}')
+def update_category(category_id: str, data: CategoryCreate) -> Category:
+    for category in categories:
+        if category.id == category_id:
+            category.name = data.name
+            return category
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Категория не найдена",
+    )
+
+@app.delete('/categories/{category_id}', status_code=status.HTTP_204_NO_CONTENT, response_class=Response)
+def delete_category(category_id: str):
+    for category in categories:
+        if category.id == category_id:
+            categories.remove(category)
+            return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Категория не найдена",
+    )
